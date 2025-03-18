db732b07698f9ad460b94a9758af6cc3592b751e
Vineesh Chauhan <chauhanvineesh@gmail.com>
2025-03-19 00:27:35 +0530

    19032025-PythonDay9

--- PyShop/products/models.py
+++ PyShop/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+    objects = models.Manager()  # Ensure default manager exists
+
+class Offer(models.Model):
+    code = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    discount = models.FloatField()
